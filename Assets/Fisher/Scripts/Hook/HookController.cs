3f7e5890fdd2a569d4c813a396001089b652b816
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2021-07-03 18:49:43 +0300

    +add DOTween to project +refactor Hook controller

--- Assets/Fisher/Scripts/Hook/HookController.cs
+++ Assets/Fisher/Scripts/Hook/HookController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using FisherMan.PlayerInput;
+using System;
 
 namespace FisherMan.Hook
 {
@@ -15,7 +16,10 @@ namespace FisherMan.Hook
         [SerializeField] private Collider2D _colider2D;
 
 
+        //DOTween Adjusting
         private Tweener _cameraTween;
+        private Sequence _sequence;
+
 
         private bool canMove = true;
 
@@ -23,7 +27,7 @@ namespace FisherMan.Hook
 
         private void Update()
         {
-            if(canMove && _inputData.FishingClick)
+            if (canMove && _inputData.FishingClick)
             {
                 float mouseXPosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
                 TransformMoveX(mouseXPosition);
@@ -37,7 +41,55 @@ namespace FisherMan.Hook
 
         public void StartFishing()
         {
-            float time = (-_hookControllerSettings.Length) * 0.1f;
+            float timeMultiplier = 0.1f;
+            float time = (-_hookControllerSettings.Length) * timeMultiplier;
+
+            _cameraTween = _mainCamera.transform.DOMoveY(_hookControllerSettings.Length, 1 + time * 0.25f, false).OnUpdate(delegate
+            {
+                float exitYCord = -11f;
+                if (_mainCamera.transform.position.y <= exitYCord)
+                    transform.SetParent(_mainCamera.transform);
+            }).OnComplete(delegate
+            {
+                _colider2D.enabled = true;
+                _cameraTween = _mainCamera.transform.DOMoveY(0, time * 5, false).OnUpdate(delegate
+                    {
+                        float endYCord = -25f;
+                        if (_mainCamera.transform.position.y >= endYCord)
+                        {
+                            StopFishing();
+                        }
+                    });
+
+
+            });
+
+
+            _colider2D.enabled = false;
+            canMove = true;
+
+        }
+
+        private void StopFishing()
+        {
+            canMove = false;
+            _cameraTween.Kill(false);
+            _cameraTween = _mainCamera.transform.DOMoveY(0, 2, false).OnUpdate(delegate
+            {
+                float bottomEntryYcord = -11f;
+                float defaultYCord = -6;
+
+                if (_mainCamera.transform.position.y >= bottomEntryYcord)
+                {
+                    transform.SetParent(null);
+                    transform.position = new Vector2(transform.position.x, defaultYCord);
+                }
+            }).OnComplete(delegate
+            {
+                transform.position = Vector2.down * 6;
+                _colider2D.enabled = true;
+                int num = 0;
+            });
         }
     }
 }
