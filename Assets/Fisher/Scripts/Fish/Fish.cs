f6d703a3770c0c2cda7cbd27675c68eafb54f816
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2021-07-04 14:06:11 +0300

    +refactor fish controller

---
+++ Assets/Fisher/Scripts/Fish/Fish.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace FisherMan.Fish
+{
+    public class Fish : MonoBehaviour
+    {
+        [SerializeField] private CircleCollider2D _circleColl;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        private float _screenLeft;
+
+        private Tweener _tweener;
+
+        private FishType _fishType;
+        public FishType fishType
+        {
+            get
+            {
+                return _fishType;
+            }
+            set
+            {
+                _fishType = value;
+                _circleColl.radius = _fishType.colliderRadius;
+                _spriteRenderer.sprite = _fishType.sprite;
+            }
+        }
+
+        private void Awake()
+        {
+            _screenLeft = Camera.main.ScreenToWorldPoint(Vector3.zero).x;
+
+        }
+
+
+
+
+        public void ResetFish()
+        {
+            if (_tweener != null) _tweener.Kill();
+
+            //Random float for fish y cord
+            float num = UnityEngine.Random.Range(_fishType.minLenght, _fishType.maxLenght);
+            _circleColl.enabled = true;
+
+            Vector3 position = transform.position;
+            position.y = num;
+            position.x = _screenLeft;
+            transform.position = position;
+
+            float num2 = 1;
+            float y = UnityEngine.Random.Range(num - num2, num + num2);
+            Vector2 v = new Vector2(-position.x, y);
+
+            float num3 = 3;
+            float delay = UnityEngine.Random.Range(0, 2 * num3);
+            _tweener = transform.DOMove(v, num3, false).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear).SetDelay(delay).OnStepComplete(delegate
+            {
+                Vector3 localScale = transform.localScale;
+                localScale.x = -localScale.x;
+                transform.localScale = localScale;
+            });
+
+        }
+
+        public void Hooked()
+        {
+            _circleColl.enabled = false;
+            _tweener.Kill();
+        }
+
+
+        [Serializable]
+        public class FishType
+        {
+            public int price;
+            public float fishCount;
+            public float minLenght;
+            public float maxLenght;
+            public float colliderRadius;
+            public Sprite sprite;
+        }
+    }
+}
